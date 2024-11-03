70d721bf646af2bd4a8f915453c15730ffb629e5
s22524385 <s225214385@mandela.ac.za>
2024-11-03 11:59:30 +0200

    fixed screen display, need to now show map

--- app/src/main/java/com/example/ek/ClientMapFragment.java
+++ app/src/main/java/com/example/ek/ClientMapFragment.java
@@ -1,23 +1,15 @@
 package com.example.ek;
 
-import static com.google.android.gms.maps.SupportMapFragment.*;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ClientMapFragment extends Fragment {
@@ -37,25 +29,19 @@ public class ClientMapFragment extends Fragment {
                 getChildFragmentManager().findFragmentById(R.id.google_map);
 
         // assert supportMapFragment != null;
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull GoogleMap googleMap) {
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(@NonNull LatLng latLng) {
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                        markerOptions.title(latLng.latitude + ":" + latLng.longitude);
-                        googleMap.clear();
+        if (supportMapFragment != null) {
+            supportMapFragment.getMapAsync(googleMap -> googleMap.setOnMapClickListener(latLng -> {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + ":" + latLng.longitude);
+                googleMap.clear();
 
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                        googleMap.addMarker(markerOptions);
-                    }
-                });
-            }
-        });
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                googleMap.addMarker(markerOptions);
+            }));
+        }
 
-        NavController navController = Navigation.findNavController(getActivity(), R.id.navHostFragmentContainerView);
+        // NavController navController = Navigation.findNavController(getActivity(), R.id.navHostFragmentContainerView);
 
         return view;
     }
